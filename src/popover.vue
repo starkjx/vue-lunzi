3a1805bd13103c31d3427d48493c17fec0bf9a27
starkjx <303245803@qq.com>
2019-03-16 15:51:12 +0800

    创建popover组件

---
+++ src/popover.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="popover" @click="xxx">
+    <div v-if="visible" class="content-wrapper">
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'LunziPopover',
+    data(){
+      return {
+        visible: false
+      }
+    },
+    methods:{
+      xxx(){
+        this.visible = !this.visible
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .popover{
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+    .content-wrapper{
+      position: absolute;
+      bottom:100%;
+      left:0;
+      border: 1px solid red;
+      box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    }
+  }
+
+</style>
